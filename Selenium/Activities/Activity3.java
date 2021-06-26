d00a084492ec2b53035a243c329da613da359bc7
priyankasolunke <85600703+priyankasolunke@users.noreply.github.com>
2021-06-26 13:57:18 +0530

    Activities

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,31 @@
+package SeleniumActivitiesFST;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity3 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+    
+        driver.get("https://www.training-support.net/selenium/simple-form");
+        
+        //Find the page title and print it
+        String pageTitle = driver.getTitle();
+        System.out.println(pageTitle);
+        
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        //Enter text
+        firstName.sendKeys("Pihu");
+        lastName.sendKeys("Solunke");
+        
+        driver.findElement(By.id("email")).sendKeys("test@example.com");
+        driver.findElement(By.id("number")).sendKeys("1234567890");
+        driver.findElement(By.cssSelector(".ui.green.button")).click();
+    
+        driver.close();
+    }
+}
